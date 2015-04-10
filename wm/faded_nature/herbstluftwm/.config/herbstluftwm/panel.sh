b96081714c3d1de928c7364bbfd5f8a069e05313
John Xue <Xuejohn369@gmail.com>
2015-04-10 19:24:31 -0400

    new experimental panel

---
+++ wm/faded_nature/herbstluftwm/.config/herbstluftwm/panel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BG="#ff09110a"
+BG1="#ff1e3921"
+COL2="#ff76874d"
+COL1="#ffad6031"
+FG="#ff95b462"
+COL3="#ffe8c936"
+FONT="-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso8859-1"
+
+date1(){
+        DATE1=$(date '+%I:%M')
+        echo "$DATE1"
+}
+
+date2(){
+        DATE2=$(date '+%a %d %b')
+        echo "$DATE2"
+}
+
+work(){
+	WORKSPACE=$(xprop -root -notype _NET_CURRENT_DESKTOP | egrep -o "[0-9]");
+	echo "$WORKSPACE"
+}
+
+vol(){
+        VOL=$(amixer get Master | egrep -o "[0-9]+%" | egrep -o "[0-9]*")
+        echo "$VOL"
+}
+
+while true; do
+        echo "%{l}%{B$BG1}%{F$FG} $(work) %{B-}  %{F$COL1}$(vol)  %{F$COL2}$(date2)  %{F$COL3}$(date1)  "
+        sleep 0.1;
+done | lemonbar -f "$FONT" -B "$BG" -g "200x20+20+20"
