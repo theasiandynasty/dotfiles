89b093f323d2149e3ddf0cd792fc99de7fce71ba
John Xue <Xuejohn369@gmail.com>
2015-05-20 22:42:54 -0400

    nightly commit

--- wm/faded_nature/herbstluftwm/.config/herbstluftwm/dmenu.sh
+++ wm/faded_nature/herbstluftwm/.config/herbstluftwm/dmenu.sh
@@ -1,2 +1,9 @@
-MUSIC=$(mpc current -f "%title%")
-dmenu_run -i -noinput -x 240 -y 20 -h 20 -w 600 -p "$MUSIC" -nb "#09110A" -nf "#76874D" -sb "#1E3921" -sf "#95B462"
+TITLE=$(mpc current)
+
+if [ -z $TITLE ]; then
+    TITLE="exec_"
+else
+    TITLE=$(mpc current)
+fi
+
+dmenu_run -i -noinput -x 240 -y 20 -h 20 -w 600 -p "$TITLE" -nb "#09110A" -nf "#76874D" -sb "#1E3921" -sf "#95B462"
